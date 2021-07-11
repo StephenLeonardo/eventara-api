0d99aa01170dbb1d0a966ef7e3f8fdc2b8b1fa26
Stephen Leonardo <stephenleonardo18@gmail.com>
2021-07-11 13:12:05 +0700

    Change default time format

--- evehuntAPI/settings.py
+++ evehuntAPI/settings.py
@@ -92,6 +92,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
     'NON_FIELD_ERRORS_KEY': 'Error',
+    'TIME_FORMAT': "%H:%M",
 }
 #
 # from datetime import timedelta
