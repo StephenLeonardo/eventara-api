b3cc936b31f5e6c75ecbc2e495c12f6b6388f7b9
Stephen <stephenleonardo18@gmail.com>
2021-08-18 19:12:07 +0700

    change project name to eventaraAPI & update gitignore

--- eventaraAPI/asgi.py
+++ eventaraAPI/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for evehuntAPI project.
+ASGI config for eventaraAPI project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'evehuntAPI.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventaraAPI.settings')
 
 application = get_asgi_application()
